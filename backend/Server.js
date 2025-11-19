63563a0f687893ccb5d02f67da390be650f33f89
Anila Nawaz <anilanawaz531@gmail.com>
2025-11-19 20:17:22 +0500

    fixed a production error

--- backend/Server.js
+++ backend/Server.js
@@ -40,7 +40,7 @@ const path = require('path');
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../mestore/dist')));
 
-  app.get('*', (req, res) => {
+  app.get('/*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../mestore/dist/index.html'));
   });
 }
